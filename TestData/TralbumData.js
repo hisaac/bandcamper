a405451972514dd423c613df501cb0022ca88eca
Isaac Halvorson <hello@hisaac.net>
2018-08-06 15:24:30 -0500

    Add test data

---
+++ TestData/TralbumData.js
@@ -0,0 +1,146 @@
+var TralbumData = {
+	// For the curious:
+	// http://bandcamp.com/help/audio_basics#steal
+	// http://bandcamp.com/terms_of_use
+	current: {
+		"type": "album",
+		"art_id": 3499381153,
+		"set_price": 7.0,
+		"publish_date": "28 Nov 2013 19:31:45 GMT",
+		"upc": null,
+		"band_id": 834764491,
+		"new_desc_format": 1,
+		"minimum_price_nonzero": 7.0,
+		"auto_repriced": null,
+		"minimum_price": 7.0,
+		"title": "Dobie - The Sound of One Hand Clapping (Deluxe Edition)",
+		"purchase_url": null,
+		"download_desc_id": null,
+		"credits": "Produced by Dobie",
+		"require_email": null,
+		"mod_date": "06 Aug 2018 00:01:25 GMT",
+		"audit": 0,
+		"artist": "Dobie",
+		"is_set_price": null,
+		"purchase_title": null,
+		"require_email_0": null,
+		"killed": null,
+		"selling_band_id": 834764491,
+		"about": "Originally released in 1998\r\nDeluxe Edition includes two bonus tracks",
+		"download_pref": 2,
+		"private": null,
+		"new_date": "28 Nov 2013 17:22:30 GMT",
+		"featured_track_id": 418792464,
+		"id": 4190276032,
+		"release_date": "28 Nov 2013 00:00:00 GMT"
+	},
+	is_preorder: null,
+	album_is_preorder: null,
+	album_release_date: "28 Nov 2013 00:00:00 GMT",
+	preorder_count: null,
+	hasAudio: true,
+	art_id: 3499381153,
+	trackinfo: [
+		{
+			"video_caption": null,
+			"is_draft": false,
+			"lyrics": null,
+			"title_link": "/track/connectivity-feat-roots-manuva-the-hidden-mix",
+			"file": {
+				"mp3-v0": "https://t4.bcbits.com/stream/99c7854477a5da4012131cf0c888b38b/mp3-v0/418792464?p=1&ts=1533524038&t=dcd8473466948924d7aac85e535c874649fc7eee&token=1533524038_502041e6860382bdbbd1a6d8213e0d6348acdd7b",
+				"mp3-128": "https://bandcamp.com/stream_redirect?enc=mp3-128&track_id=418792464&ts=1533520438&t=a37ff0b7f5c972887b60bd35a606287636de9faa"
+			},
+			"is_capped": null,
+			"sizeof_lyrics": 0,
+			"video_featured": null,
+			"video_id": null,
+			"streaming": 1,
+			"has_lyrics": false,
+			"license_type": 1,
+			"title": "Connectivity (feat. Roots Manuva) The Hidden Mix",
+			"video_source_type": null,
+			"alt_link": null,
+			"has_info": true,
+			"video_source_id": null,
+			"track_num": 1,
+			"encoding_error": null,
+			"is_downloadable": true,
+			"video_mobile_url": null,
+			"album_preorder": false,
+			"private": null,
+			"encoding_pending": null,
+			"duration": 309.847,
+			"has_free_download": null,
+			"video_poster_url": null,
+			"unreleased_track": false,
+			"track_id": 418792464,
+			"id": 418792464,
+			"track_license_id": null,
+			"play_count": null,
+			"free_album_download": false,
+			"encodings_id": 1652952967
+		},
+		{
+			"video_caption": null, "is_draft": false, "lyrics": null, "title_link": "/track/consider-feat-rodney-p", "file": { "mp3-v0": "https://t4.bcbits.com/stream/591b3da764d330ed4760ce30e971aeac/mp3-v0/3274852795?p=1&ts=1533524038&t=15fdfb46adafc493c8c1b2b16cc47b03706ff18c&token=1533524038_b4236578b3aaf95ccbd38736947dc51354168e9e", "mp3-128": "https://bandcamp.com/stream_redirect?enc=mp3-128&track_id=3274852795&ts=1533520438&t=b28aab2330ae9bf6b9e5d1c63ffbda8ab48a6efd" }, "is_capped": null, "sizeof_lyrics": 0, "video_featured": null, "video_id": null, "streaming": 1, "has_lyrics": false, "license_type": 1, "title": "Consider (feat Rodney P)", "video_source_type": null, "alt_link": null, "has_info": true, "video_source_id": null, "track_num": 2, "encoding_error": null, "is_downloadable": true, "video_mobile_url": null, "album_preorder": false, "private": null, "encoding_pending": null, "duration": 355.916, "has_free_download": null, "video_poster_url": null, "unreleased_track": false, "track_id": 3274852795, "id": 3274852795, "track_license_id": null, "play_count": null, "free_album_download": false, "encodings_id": 3304833725
+		},
+		{
+			"video_caption": null, "is_draft": false, "lyrics": null, "title_link": "/track/the-ride-feat-lain-gray", "file": { "mp3-v0": "https://t4.bcbits.com/stream/842869bff5de336668aba609337152d5/mp3-v0/397248784?p=1&ts=1533524038&t=1f3c0d8855f52aefaf6f0165126371c41866963f&token=1533524038_8e6f82c0ec7f9e2c56faa1888ebaeabd3e5761c4", "mp3-128": "https://bandcamp.com/stream_redirect?enc=mp3-128&track_id=397248784&ts=1533520438&t=f3cc66d91b47a6db5bb26449e8418281f0dfac39" }, "is_capped": null, "sizeof_lyrics": 0, "video_featured": null, "video_id": null, "streaming": 1, "has_lyrics": false, "license_type": 1, "title": "The Ride (feat. Lain Gray)", "video_source_type": null, "alt_link": null, "has_info": true, "video_source_id": null, "track_num": 3, "encoding_error": null, "is_downloadable": true, "video_mobile_url": null, "album_preorder": false, "private": null, "encoding_pending": null, "duration": 252.633, "has_free_download": null, "video_poster_url": null, "unreleased_track": false, "track_id": 397248784, "id": 397248784, "track_license_id": null, "play_count": null, "free_album_download": false, "encodings_id": 991438151
+		},
+		{
+			"video_caption": null, "is_draft": false, "lyrics": null, "title_link": "/track/radox-feat-ninety-9", "file": { "mp3-v0": "https://t4.bcbits.com/stream/cd7f9c619520f49eb35c18bb4d467dd0/mp3-v0/3717131337?p=1&ts=1533524038&t=25b64dc74b8b35891a77fc91f666761a651eee7f&token=1533524038_7f0af12d33eee19b2bd3cb46d5bf4ef6b9b8273c", "mp3-128": "https://bandcamp.com/stream_redirect?enc=mp3-128&track_id=3717131337&ts=1533520438&t=aaf3331217dc6fdcad05bf939e1d8a1289f66161" }, "is_capped": null, "sizeof_lyrics": 0, "video_featured": null, "video_id": null, "streaming": 1, "has_lyrics": false, "license_type": 1, "title": "Radox (feat. Ninety-9)", "video_source_type": null, "alt_link": null, "has_info": true, "video_source_id": null, "track_num": 4, "encoding_error": null, "is_downloadable": true, "video_mobile_url": null, "album_preorder": false, "private": null, "encoding_pending": null, "duration": 286.813, "has_free_download": null, "video_poster_url": null, "unreleased_track": false, "track_id": 3717131337, "id": 3717131337, "track_license_id": null, "play_count": null, "free_album_download": false, "encodings_id": 548786130
+		},
+		{
+			"video_caption": null, "is_draft": false, "lyrics": null, "title_link": "/track/love-song-feat-helena-paul", "file": { "mp3-v0": "https://t4.bcbits.com/stream/618d71d4c2e33890a8793746710fc54a/mp3-v0/4199612993?p=1&ts=1533524038&t=49844629775e43faac2a8c7d6aae6473a7c29913&token=1533524038_1d69595c3454520a840813b6a778fe81d2f12594", "mp3-128": "https://bandcamp.com/stream_redirect?enc=mp3-128&track_id=4199612993&ts=1533520438&t=1f0ae7afbbbec7b9d4e6bc663d0dfa6123bf80a7" }, "is_capped": null, "sizeof_lyrics": 0, "video_featured": null, "video_id": null, "streaming": 1, "has_lyrics": false, "license_type": 1, "title": "Love Song (feat. Helena Paul)", "video_source_type": null, "alt_link": null, "has_info": true, "video_source_id": null, "track_num": 5, "encoding_error": null, "is_downloadable": true, "video_mobile_url": null, "album_preorder": false, "private": null, "encoding_pending": null, "duration": 283.841, "has_free_download": null, "video_poster_url": null, "unreleased_track": false, "track_id": 4199612993, "id": 4199612993, "track_license_id": null, "play_count": null, "free_album_download": false, "encodings_id": 1529971945
+		},
+		{
+			"video_caption": null, "is_draft": false, "lyrics": null, "title_link": "/track/coming-up-for-air-feat-rassia", "file": { "mp3-v0": "https://t4.bcbits.com/stream/0a57aa69e1b3b0853bb930e5d21609e4/mp3-v0/4049100461?p=1&ts=1533524038&t=a4e2c394e230e966ba04600a8d935c12264ab463&token=1533524038_e81c3166e8cc97ad51fffb180c841df5c1afeead", "mp3-128": "https://bandcamp.com/stream_redirect?enc=mp3-128&track_id=4049100461&ts=1533520438&t=156d9c940be6a799100bda908b349805be226118" }, "is_capped": null, "sizeof_lyrics": 0, "video_featured": null, "video_id": null, "streaming": 1, "has_lyrics": false, "license_type": 1, "title": "Coming Up For Air (feat. Rassia)", "video_source_type": null, "alt_link": null, "has_info": true, "video_source_id": null, "track_num": 6, "encoding_error": null, "is_downloadable": true, "video_mobile_url": null, "album_preorder": false, "private": null, "encoding_pending": null, "duration": 385.637, "has_free_download": null, "video_poster_url": null, "unreleased_track": false, "track_id": 4049100461, "id": 4049100461, "track_license_id": null, "play_count": null, "free_album_download": false, "encodings_id": 1446390941
+		},
+		{
+			"video_caption": null, "is_draft": false, "lyrics": null, "title_link": "/track/b-boy-anthem-pt-2-instrumental", "file": { "mp3-v0": "https://t4.bcbits.com/stream/51129e2544db1b36442efea2a33391b9/mp3-v0/1313601503?p=1&ts=1533524038&t=ac64318b21081bb60c955971fb7a99bbc8af0e8b&token=1533524038_392650e1a4699beabbe69167e6483cc9bd33bba7", "mp3-128": "https://bandcamp.com/stream_redirect?enc=mp3-128&track_id=1313601503&ts=1533520438&t=c547ef60f4a78d422ade9b5edca79c05447d95ae" }, "is_capped": null, "sizeof_lyrics": 0, "video_featured": null, "video_id": null, "streaming": 1, "has_lyrics": false, "license_type": 1, "title": "B-Boy Anthem Pt. 2 (Instrumental)", "video_source_type": null, "alt_link": null, "has_info": true, "video_source_id": null, "track_num": 7, "encoding_error": null, "is_downloadable": true, "video_mobile_url": null, "album_preorder": false, "private": null, "encoding_pending": null, "duration": 278.64, "has_free_download": null, "video_poster_url": null, "unreleased_track": false, "track_id": 1313601503, "id": 1313601503, "track_license_id": null, "play_count": null, "free_album_download": false, "encodings_id": 2669168212
+		},
+		{
+			"video_caption": null, "is_draft": false, "lyrics": null, "title_link": "/track/love-hate-feat-rodney-p-done-e", "file": { "mp3-v0": "https://t4.bcbits.com/stream/4d7a195d99d2b4fbe1ed756032929650/mp3-v0/1903909359?p=1&ts=1533524038&t=c16797b99cdee74f7d9af881c6f7c7160f5d3d45&token=1533524038_74f233950c6575bf9798d3161d8cbca5ee522b58", "mp3-128": "https://bandcamp.com/stream_redirect?enc=mp3-128&track_id=1903909359&ts=1533520438&t=81e64688f613b55df1bf90928407b75fb4565c0b" }, "is_capped": null, "sizeof_lyrics": 0, "video_featured": null, "video_id": null, "streaming": 1, "has_lyrics": false, "license_type": 1, "title": "Love & Hate (feat. Rodney P & Done-E)", "video_source_type": null, "alt_link": null, "has_info": true, "video_source_id": null, "track_num": 8, "encoding_error": null, "is_downloadable": true, "video_mobile_url": null, "album_preorder": false, "private": null, "encoding_pending": null, "duration": 286.07, "has_free_download": null, "video_poster_url": null, "unreleased_track": false, "track_id": 1903909359, "id": 1903909359, "track_license_id": null, "play_count": null, "free_album_download": false, "encodings_id": 3011108533
+		},
+		{
+			"video_caption": null, "is_draft": false, "lyrics": null, "title_link": "/track/way-over-instrumental", "file": { "mp3-v0": "https://t4.bcbits.com/stream/8c2207dd9d4bb74ea1b521bf7ac30573/mp3-v0/204159171?p=1&ts=1533524038&t=248bc802c4b6914bd039239c0f0d28e8d9511058&token=1533524038_7a975663338ab253f8ecfd77f04d3d24da743121", "mp3-128": "https://bandcamp.com/stream_redirect?enc=mp3-128&track_id=204159171&ts=1533520438&t=d369c6ab38aad39779c1fc76edad5cd5e6731c64" }, "is_capped": null, "sizeof_lyrics": 0, "video_featured": null, "video_id": null, "streaming": 1, "has_lyrics": false, "license_type": 1, "title": "Way Over (Instrumental)", "video_source_type": null, "alt_link": null, "has_info": true, "video_source_id": null, "track_num": 9, "encoding_error": null, "is_downloadable": true, "video_mobile_url": null, "album_preorder": false, "private": null, "encoding_pending": null, "duration": 357.402, "has_free_download": null, "video_poster_url": null, "unreleased_track": false, "track_id": 204159171, "id": 204159171, "track_license_id": null, "play_count": null, "free_album_download": false, "encodings_id": 1062071448
+		},
+		{
+			"video_caption": null, "is_draft": false, "lyrics": null, "title_link": "/track/cloud-98-feat-ninety-9", "file": { "mp3-v0": "https://t4.bcbits.com/stream/459cca8f49d33054f19085cb54fdbd57/mp3-v0/2120258971?p=1&ts=1533524038&t=d104cba2a917603423d89a527ded1f8c0e2ad63b&token=1533524038_9a8f13bdf1df6fc0b31aadeca25acc102e96962b", "mp3-128": "https://bandcamp.com/stream_redirect?enc=mp3-128&track_id=2120258971&ts=1533520438&t=437046ac37066165e347569e3196d550e0a8bfad" }, "is_capped": null, "sizeof_lyrics": 0, "video_featured": null, "video_id": null, "streaming": 1, "has_lyrics": false, "license_type": 1, "title": "Cloud 98 (feat. Ninety-9)", "video_source_type": null, "alt_link": null, "has_info": true, "video_source_id": null, "track_num": 10, "encoding_error": null, "is_downloadable": true, "video_mobile_url": null, "album_preorder": false, "private": null, "encoding_pending": null, "duration": 279.383, "has_free_download": null, "video_poster_url": null, "unreleased_track": false, "track_id": 2120258971, "id": 2120258971, "track_license_id": null, "play_count": null, "free_album_download": false, "encodings_id": 1945688816
+		}
+	],
+	playing_from: "album page",
+	featured_track_id: 418792464,
+	initial_track_num: null,
+	packages: null,
+	url: "http://dobie.bandcamp.com" + "/album/dobie-the-sound-of-one-hand-clapping-deluxe-edition",
+	defaultPrice: 7.0,
+	freeDownloadPage: null,
+	FREE: 1,
+	PAID: 2,
+	artist: "Dobie",
+	item_type: "album", // xxx: note - don't internationalize this variable
+	id: 4190276032,
+	last_subscription_item: null,
+	has_discounts: null,
+	is_bonus: null,
+	play_cap_data: null,
+	client_id_sig: null,
+	is_purchased: true,
+	items_purchased: { "packages": {}, "bundles": {} },
+	is_private_stream: null,
+	is_band_member: null,
+	licensed_version_ids: null,
+	package_associated_license_id: null,
+	tralbum_collect_info: {
+		"show_collect": true,
+		"fan_trackpipe_url": "https://bandcamp.com/hisaac",
+		"show_wishlist_tooltip": null,
+		"is_collected": true,
+		"collect_band_id": 834764491,
+		"fan_id": 1233,
+		"collect_item_id": 4190276032,
+		"part_of_purchased_album": false,
+		"collect_item_type": "album",
+		"is_purchased": true
+	}
+};
