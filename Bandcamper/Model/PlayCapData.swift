8dbb4d30d3f5d43c92f06edd51048a63e3751e53
Isaac Halvorson <hello@hisaac.net>
2018-09-07 14:07:03 -0500

    Clean up project structure

--- Bandcamper/Model/PlayCapData.swift
+++ Bandcamper/Model/PlayCapData.swift
@@ -1,10 +1,10 @@
 // Created by Isaac Halvorson on 8/15/18
 
-struct PlayCapData: Decodable {
-	private(set) var streamingLimit: Int?
-	private(set) var streamingLimitsEnabled: Bool?
+struct PlayCapData: Codable {
+	let streamingLimit: Int
+	let streamingLimitsEnabled: Bool
 
-	private enum CodingKeys: String, CodingKey {
+	enum CodingKeys: String, CodingKey {
 		case streamingLimit = "streaming_limit"
 		case streamingLimitsEnabled = "streaming_limits_enabled"
 	}
