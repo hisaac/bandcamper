2cbffd3febc3c395a4290155bb8af1344380c4e9
Isaac Halvorson <hello@hisaac.net>
2018-09-07 14:09:27 -0500

    Update TralbumCollectInfo

--- Bandcamper/Model/TralbumCollectInfo.swift
+++ Bandcamper/Model/TralbumCollectInfo.swift
@@ -1,10 +1,9 @@
 // Created by Isaac Halvorson on 8/15/18
 
-struct TralbumCollectInfo: Decodable {
-	private(set) var showCollect: Bool?
-	private(set) var showWishlistTooltip: Bool?
+struct TralbumCollectInfo: Codable {
+	let showCollect, showWishlistTooltip: Bool
 
-	private enum CodingKeys: String, CodingKey {
+	enum CodingKeys: String, CodingKey {
 		case showCollect = "show_collect"
 		case showWishlistTooltip = "show_wishlist_tooltip"
 	}
